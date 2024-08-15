f5ef6d48222da1a864c9bcc3e8e039d0c5756e68
obieh <o_amadic@yahoo.com>
2024-08-15 14:01:43 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-document.querySelector('.check').addEventListener('click', function () {
-    console.log(document.querySelector('.guess').value)
+'use strict'
+
+const secretNumber = Math.trunc(Math.random()*20+1)
+
+document.querySelector('secretNumber').textContent = secretNumber
+
+document.querySelector('.check').addEventListener
+('click', function () {
+   console.log(document.querySelector('.guess').value)
+   console.log(guess, typeof guess)
+    
+   if(!guess){
+    document.querySelector('message').textContent = 'No secretNumber!'
+   }else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number'
+   }
 })
- 
+
+
+    
+
+
+
