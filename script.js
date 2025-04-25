eef6830136c413655f22950666ad61ad0035c2f5
obieh <o_amadic@yahoo.com>
2025-04-25 10:02:09 +0100

    update main commit

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 'use strict'
 
+//let user = prompt('please enter your name ')
+//document.querySelector('.mem').textContent = `Welcome ${user} to my guessing game`
+
 const secretNumber = Math.trunc(Math.random()*20+1)
 
-document.querySelector('secretNumber').textContent = secretNumber
+//document.querySelector('.secretNumber').textContent = secretNumber
 
 document.querySelector('.check').addEventListener
 ('click', function () {
-   console.log(document.querySelector('.guess').value)
+   const guess = Number(document.querySelector('.guess').value)
    console.log(guess, typeof guess)
     
    if(!guess){
-    document.querySelector('message').textContent = 'No secretNumber!'
+    document.querySelector('.message').textContent = '⛔️ No number!'
    }else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number'
+    document.querySelector('.message').textContent = '🎉 Correct Number'
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').textContent = secretNumber
+    document.querySelector('.number').style.width = '26rem'
+   }else if(guess > secretNumber) {
+    document.querySelector('.message').textContent= "📈 Too high"
+   }else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = "📉 Too low"
    }
 })
 
